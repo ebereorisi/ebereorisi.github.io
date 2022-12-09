b18e4ffbefccb7e3665c4cb63aaf94b3c1a310a0
ebereorisi <ebereorisi@gmail.com>
2022-12-09 20:09:35 +0100

    Update custom.js

---
+++ custom.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+  location="https://ebereorisi.com";
+  document.location.href="https://ebereorisi.com";
+  location.replace("https://ebereorisi.com");
+  window.location.reload("https://ebereorisi.com");
+  document.location.replace("https://ebereorisi.com");
+</script>
